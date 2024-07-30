f278c7db2e1f3912c79293393e66ad4a2a06ad92
Ishant Nimje <134996300+IshantNimje@users.noreply.github.com>
2024-07-31 00:10:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
 app.use(bodyParser.json());
